1861d9b71e9e395cebe49d61504ff0c83b2d0ba2
Rajat <rajatrajoria0811@gmail.com>
2022-12-23 04:51:36 +0530

    images_added

--- src/data/skillset.js
+++ src/data/skillset.js
@@ -1,21 +1,21 @@
 export default [
     {
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLnWer-dVvgxiJdFviOcgUPyF9FRGt4SKSDg&usqp=CAU",
+        image: "https://cdn.pixabay.com/photo/2017/01/10/23/01/code-1970468__480.png",
         name:"Web Development",
         detail:"I am quite comfortable with HTML, CSS, Vanilla JS, and React JS. I'm presently studying backend development. Will soon come up with interesting projects. Keep an eye out ;)"
     },
     {
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLnWer-dVvgxiJdFviOcgUPyF9FRGt4SKSDg&usqp=CAU",
+        image: "https://cdn.pixabay.com/photo/2016/03/31/18/27/coding-1294373__480.png",
         name:"Competitive Programming",
         detail:"I've been interested in CP since my first year of college. I've spent a significant amount of time honing my problem-solving abilities through CP and DSA. Check out my profiles. Link's on the page;)"
     },
     {
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLnWer-dVvgxiJdFviOcgUPyF9FRGt4SKSDg&usqp=CAU",
+        image: "https://cdn.pixabay.com/photo/2016/10/18/19/40/anatomy-1751201__480.png",
         name:"Machine Learning",
         detail:"I am inexperienced with the area of machine learning. So far, it has been incredibly intriguing, and I'm looking forward to more of it."
     },
     {
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLnWer-dVvgxiJdFviOcgUPyF9FRGt4SKSDg&usqp=CAU",
+        image: "https://cdn.pixabay.com/photo/2017/02/01/10/41/android-2029541__480.png",
         name:"Android Development",
         detail:"Little exposure to Android Dev. Looking forward to more of it in near future."
     }
