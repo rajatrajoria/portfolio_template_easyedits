5f49d02cf868aa40e422bdfbb701dea73c91941c
Rajat <rajatrajoria0811@gmail.com>
2022-12-23 15:12:47 +0530

    side_scroll_bar_implemented

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,5 +13,15 @@ export default[
         image: "https://uploads-ssl.webflow.com/5eb98b2079b4fc247b5a89f3/5eb99a5c993b6c45ac06cc1c_Project%203-p-1600.jpeg",
         name: "vbrseusbf eubf dj",
         detail: "Hellodbfiu edjkfvbfd sufvb wedjfbubvsd vusdbf vuwsdbf vehasbf vcueabs dcvuehiabs fxvcugohieabs fxvucghieankd zfxhvchoiakns xfhvcughoieaknd sfxhvcughoiakns fnxhvchoiaken sfxvcughoiknab sfxnvgchlkna"
+    },
+    {
+        image: "https://uploads-ssl.webflow.com/5eb98b2079b4fc247b5a89f3/5eb99a5c993b6c45ac06cc1c_Project%203-p-1600.jpeg",
+        name: "vbrseusbf eubf dj",
+        detail: "Hellodbfiu edjkfvbfd sufvb wedjfbubvsd vusdbf vuwsdbf vehasbf vcueabs dcvuehiabs fxvcugohieabs fxvucghieankd zfxhvchoiakns xfhvcughoieaknd sfxhvcughoiakns fnxhvchoiaken sfxvcughoiknab sfxnvgchlkna"
+    },
+    {
+        image: "https://uploads-ssl.webflow.com/5eb98b2079b4fc247b5a89f3/5eb99a5c993b6c45ac06cc1c_Project%203-p-1600.jpeg",
+        name: "vbrseusbf eubf dj",
+        detail: "Hellodbfiu edjkfvbfd sufvb wedjfbubvsd vusdbf vuwsdbf vehasbf vcueabs dcvuehiabs fxvcugohieabs fxvucghieankd zfxhvchoiakns xfhvcughoieaknd sfxhvcughoiakns fnxhvchoiaken sfxvcughoiknab sfxnvgchlkna"
     }
 ]
