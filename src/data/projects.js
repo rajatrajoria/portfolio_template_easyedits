5f620c6a9cafa1d6639b99bbbee32db72d15d9e8
Rajat <rajatrajoria0811@gmail.com>
2023-01-18 15:12:12 +0530

    added tenzies

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,19 +2,19 @@ export default[
     {
         image: "https://i.imgur.com/REn1kl4.jpg",
         name: "Custom Meme Generator",
-        detail: "Have fun and let the photos talk now. Some truly awesome features come with this meme maker. It has practically every tool you could possibly need to create the finest memes. Why are you holding out? Think up some incredibly funny memes, and share them with your buddies.",
+        detail: "Have fun and let the photos talk now. Some truly awesome features come with this meme maker. It has practically every tool you could possibly need to create the finest memes. Why are you holding out? Think up some incredibly funny memes, and share them with your buddies.",
         link: "https://rajat-rajoria-custom-meme-generator.netlify.app/"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2016/07/07/16/46/dice-1502706__480.jpg",
+        name: "Tenzies Plus",
+        detail: "Play this simplistic yet entertaining game of the classic - Tenzies and battle for a spot on the scoreboard. This game's global database synchronisation makes it more engaging and interesting. Stop reading right now, click this, and give it a shot :)",
+        link: "https://rajat-rajoria-tenzies.netlify.app/"
     },
     {
         image: "https://cdn.pixabay.com/photo/2019/12/12/18/44/optics-4691414__480.jpg",
         name: "Sudoku Solver",
         detail: "A Sudoku solver that can finish practically any puzzle in a matter of seconds. Real time input validator with colour hints also featured. The GUI is easy to use. Making it function is thus not particularly difficult. Watch this application solve some really challenging sudokus.",
         link: "https://rajat-rajoria-sudokusolver.netlify.app/"
-    },
-    {
-        image: "https://media.tenor.com/pSQowC23bFwAAAAM/erkenci-kus-ceycey.gif",
-        name: "Coming up soon",
-        detail: "",
-        link: ""
     }
 ]
