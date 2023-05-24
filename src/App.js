ec36055cb5abde6f19da83d1c335eb64d3e5afae
Rajat <rajatrajoria0811@gmail.com>
2023-05-24 13:09:23 +0530

    snowfalls added

--- src/App.js
+++ src/App.js
@@ -9,10 +9,18 @@ import Hobbies from "./components/Hobbies/Hobbies"
 import Tools from "./components/Tools/Tools"
 import Footer from "./components/Footer/Footer"
 import Experiences from "./components/Experiences/Experiences"
+import Snowfall from 'react-snowfall'
 
 export default function App(){
     return(
         <div>
+            <Snowfall snowflakeCount= {70} color="white"
+                style={{
+                    position: 'fixed',
+                    width: '100vw',
+                    height: '100vh',
+                }}
+            />
             <Navbar/>
             <Intro/>
             <Education/>
