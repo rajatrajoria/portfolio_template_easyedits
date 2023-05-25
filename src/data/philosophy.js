f4b8c9f91730d0a9e3aec07015f11e91bcc0b3b8
rajatrajoria <74411262+rajatrajoria@users.noreply.github.com>
2023-05-25 13:15:56 +0530

    Update philosophy.js, grammar changes

--- src/data/philosophy.js
+++ src/data/philosophy.js
@@ -3,5 +3,5 @@ export default{
     // image: "https://cdn.pixabay.com/photo/2015/07/29/00/11/sunset-865310__480.jpg",
     // image: "https://cdn.pixabay.com/photo/2018/11/27/21/52/sunset-3842490__480.jpg",
     image: "https://cdn.pixabay.com/photo/2013/11/28/10/03/river-219972__480.jpg",
-    text: "Everyone, I believe, want the same thing: a connect with mankind, metaphysical serenity, and cosmic experience. I strive to comprehend these concepts via my principles of sincerity, originality, and generosity."
+    text: "Everyone, I believe, want the same thing: a connect with mankind, metaphysical serenity, and a cosmic experience. I strive to comprehend these concepts via my principles of sincerity, originality, and generosity."
 }
